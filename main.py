38f563ca7ba6aafe5f20438555a1950cd5819084
Arseny Andrushchenko <senyazaz@gmail.com>
2021-11-14 21:36:27 +0300

    Код переместился на библеотеку tkinter по пречине ее более простого освоения и понимания. Окно увеличилось до размера 750х940. Задний фон стал черным. Добавлено игровое поле (белое) с эмблеммой РГГМУ. В коде остались закоментированные фигуры для дальнейшего использования

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-import sys
+from tkinter import *
+from tkinter import messagebox
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget
+W, H = 10, 20
+TILE = 45
+GAME_RES = W * TILE, H * TILE
+RES = 750, 940
+FPS = 60
 
 
-if __name__ == '__main__':
+def on_closing():
+    #if messagebox.askokcancel("Выход из приложения", "Хотите выйти из приложения?"):
+        tk.destroy()
 
-    app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(300, 550) # Данная комагда определяет размеры окна, а команда ниже его место расположенияя
-    w.move(960-(300)/2, 540-(550)/2) #960x540 - Центр экрана (На центр экрана встает верхний левый угол)
-    w.setWindowTitle('Tetris by Ars') # Задаем заголовок окна
-    w.setWindowIcon(QIcon('icon image for tetris.png')) # Задает иконку приложения
-    w.show() # показывает само окно приложения
-    sys.exit(app.exec_())
+
+tk = Tk()
+tk.protocol("WM_DELETE_WINDOW", on_closing)
+tk.title("Тетрис РГГМУ")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+#tk.iconbitmap("Icons/icon image for tetris")
+
+canvas = Canvas(tk, width=RES[0], height=RES[1], bg="black", highlightthickness=0)
+canvas.pack()
+
+img_obj2 = PhotoImage(file="Icons/bg2.png")
+canvas.create_image(20, 20, anchor=NW, image=img_obj2)
+
+#canvas.create_oval(100, 100, 300, 300, fill="yellow", outline="")
+#canvas.create_oval(120, 120, 280, 280, fill="white", outline="")
+
+#canvas.create_rectangle(400,100,500,500, fill="lightgreen")
+#canvas.create_rectangle(420,120,480,480, fill="darkgreen", outline="")
+
+#canvas.create_text(200,500,text="Hello World!", font=("Arial", 40),fill="white")
+
+
+tk.mainloop()
